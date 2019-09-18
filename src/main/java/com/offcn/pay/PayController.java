79e53d22dbaf3ec0335a8b4e826c48ea08a61600
hcw <haha@qq.com>
2019-09-18 22:02:06 +0800

    分支：test001 新增支付add，delete方法

---
+++ src/main/java/com/offcn/pay/PayController.java
@@ -0,0 +1,10 @@
+package com.offcn.pay;
+
+public class PayController {
+    public void add(){
+        System.out.println("新增");
+    }
+    public void delete(){
+        System.out.println("删除");
+    }
+}

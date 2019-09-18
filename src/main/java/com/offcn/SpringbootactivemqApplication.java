d9aa4f5c3cdf2654426378ddeb5a2ba762664ee9
hcw <haha@qq.com>
2019-09-18 21:49:44 +0800

    新增注释

--- src/main/java/com/offcn/SpringbootactivemqApplication.java
+++ src/main/java/com/offcn/SpringbootactivemqApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootactivemqApplication {
-
+    //注释
     public static void main(String[] args) {
         SpringApplication.run(SpringbootactivemqApplication.class, args);
     }
